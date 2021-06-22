f4c45812d70a84c361b9bde2d6831950eb3c3e47
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-21 18:20:19 -0600

    finished step 1 on checklist

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+from asu_requests import *
 
-
-# Returns (seats_open[int], total_seats[int])
-def get_course_seats(url):
-
-    # Set cookies parameter for BeautifulSoup
-    cookies = {"onlineCampusSelection": "O"}
-    soup = BeautifulSoup(requests.get(url, cookies=cookies).content, "html.parser")
-
-    # Grab the string of available seats (will return 'x of y' string, ie 12 of 100)
-    results = soup.select_one(".availableSeatsColumnValue").get_text(strip=True, separator=" ")
-
-    # Retrieve the two ints (available and total seats)
-    split_results = results.split(' ')
-    final_variables = (int(split_results[0]), int(split_results[2]))
-
-    return final_variables
-
-
-def generate_page_url(class_num):
-
-    return f'https://webapp4.asu.edu/catalog/myclasslistresults?t=2217&k={class_num}&hon=F&promod=F&c=ASUONLINE&e=all&page=1'
+test_num = '79462'
 
 
 if __name__ == '__main__':
 
     # This number is just a test
-    course_url = generate_page_url('79462')
+    course_url = generate_page_url(test_num)
     available, total = get_course_seats(course_url)
+    print(available)
+    print(total)
