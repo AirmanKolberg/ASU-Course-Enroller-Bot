85fcf5a51da1ad92ac4473ac1748cc4bf80d2990
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-21 18:57:27 -0600

    app now in-dev 1.0

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from asu_requests import *
+from twilio_functions import text_me
+from time import sleep
 
-test_num = '79462'
+# This number is just a test
+test_course_num = '79462'
 
 
 if __name__ == '__main__':
 
-    # This number is just a test
-    course_url = generate_page_url(test_num)
-    available, total = get_course_seats(course_url)
-    print(available)
-    print(total)
+    # Get URL based on course number
+    course_url = generate_page_url(test_course_num)
+    seats_available = False
+
+    while not seats_available:
+
+        # Search for available seats
+        available, total = get_course_seats(course_url)
+        response = f"{available} of {total} seats open."
+
+        # Show the results
+        print(response)
+
+        # When seats are available, notify me and end the while loop
+        if available > 0:
+
+            text_me(response)
+            seats_available = True
+
+        # Check every ten seconds
+        sleep(10)
